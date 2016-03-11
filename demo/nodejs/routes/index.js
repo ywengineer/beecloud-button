fe96b0cdd530551909c42dcaa0c24bd785d1fd2a
Chen Wei <acee06.weichen@gmail.com>
2016-03-11 10:50:47 +0800

    fix crypto to md5 utf8 for node.js

--- demo/nodejs/routes/index.js
+++ demo/nodejs/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
 	
 	var appid = "c5d1cba1-5e3f-4ba0-941d-9b0a371fe719";
 	var secret = "39a7a518-9ac8-4a9e-87bc-7885f33cf18c";
-	var title = "node.js water"
+	var title = "中文 node.js water"
 	var amount = "1"
 
 	var uuid = require('node-uuid');
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 
 	var data = appid + title + amount + outTradeNo + secret;
 	var sign = require('crypto');
-	var signStr = sign.createHash('md5').update(data).digest("hex");
+	var signStr = sign.createHash('md5').update(data, 'utf8').digest("hex");
 
   res.render('index', { title: 'JSButton', OutTradeNo: outTradeNo, Sign: signStr});
 });
