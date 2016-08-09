7544507ddb551257763d2da92ac7f0d6e3819078
ywengineer@gmail.com <ywengineer@gmail.com>
2016-08-09 19:44:50 +0800

    nodes demo

--- demo/nodejs/app.js
+++ demo/nodejs/app.js
@@ -10,13 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.set('env', 'product');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -37,24 +38,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
+} else {
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
--- demo/nodejs/routes/index.js
+++ demo/nodejs/routes/index.js
@@ -1,23 +1,29 @@
 var express = require('express');
+var uuid = require('node-uuid');
 var router = express.Router();
-
+var sign = require('crypto');
+var appid = "c244e073-bc37-43ce-974c-ae889ecaa273";
+var secret = "263db492-172d-49e2-a5b0-322ba5d04515";
+var titleMap = {
+    30: "GouQi Service For 30 Days",
+    90: "GouQi Service For 90 Days",
+    365: "GouQi Service For 365 Days",
+};
+var priceMap = {
+    30: 1100,
+    90: 3000,
+    365: 11000
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-	var appid = "c5d1cba1-5e3f-4ba0-941d-9b0a371fe719";
-	var secret = "39a7a518-9ac8-4a9e-87bc-7885f33cf18c";
-	var title = "中文 node.js water"
-	var amount = "1"
-
-	var uuid = require('node-uuid');
-	var outTradeNo = uuid.v4();
-	outTradeNo = outTradeNo.replace(/-/g, '');
-
-	var data = appid + title + amount + outTradeNo + secret;
-	var sign = require('crypto');
-	var signStr = sign.createHash('md5').update(data, 'utf8').digest("hex");
-
-  res.render('index', { title: 'JSButton', OutTradeNo: outTradeNo, Sign: signStr});
+    var days = req.query.days || 30;
+    var passport = req.query.name;
+    var title = titleMap[days];
+    var amount = '' + priceMap[days];
+    var outTradeNo = uuid.v4().replace(/-/g, '');
+    var data = appid + title + amount + outTradeNo + secret;
+    var signStr = sign.createHash('md5').update(data, 'utf8').digest("hex");
+    res.render('index', { title: 'JSButton', OutTradeNo: outTradeNo, Sign: signStr, days: days, passport: passport, title: title, amount: amount });
 });
 
 module.exports = router;
